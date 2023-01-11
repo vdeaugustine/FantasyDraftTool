708fe16e8e482e5ebc397764d10b0f49165401e4
vdeaugustine <vinnied44@csu.fullerton.edu>
2023-01-11 12:52:58 -0800

    Initial commit

---
+++ FantasyDraftTool/FantasyDraftToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  FantasyDraftToolApp.swift
+//  FantasyDraftTool
+//
+//  Created by Vincent DeAugustine on 1/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FantasyDraftToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
